1f7729482c0da243bfc912675307679dfd149103
yjl <1876986370@qq.com>
2026-01-13 11:25:39 +0800

    UI界面左下角的配置部分基本完成，除了连接状态

--- src/host.cpp
+++ src/host.cpp
@@ -1,13 +1,86 @@
 #include "host.h"
-
+#include "QFile"
+#include "QString"
+#include <QDebug>
 host::host(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui_host)
 {
     ui->setupUi(this);
+    this->setIntroduction();
+
+    QString StyleSheet;
+    QFile file_label("../resources/styles/label.qss");
+    if (file_label.open(QIODevice::ReadOnly)) {
+        StyleSheet += file_label.readAll();
+        file_label.close();
+    }
+    QFile file_lineedit("../resources/styles/lineedit.qss");
+    if (file_lineedit.open(QIODevice::ReadOnly)) {
+        StyleSheet += file_lineedit.readAll();
+        file_lineedit.close();
+    }
+    qApp->setStyleSheet(StyleSheet);
+    
+    this->connect_func_signal();
+    
+    
 }
 
 host::~host()
 {
     delete ui; 
+}
+
+void host::setIntroduction(void){
+    this->ui->label_content_company->setText("HUST");
+    this->ui->label_content_person->setText("姚建林");
+    this->ui->label_content_version->setText(this->version);
+
+}
+
+
+
+
+void host::button_confirm_clicked(void){
+    this->ui->lineEdit_dis_dream->setReadOnly(true);
+    this->ui->lineEdit_dis_max->setReadOnly(true);
+    this->ui->lineEdit_dis_min->setReadOnly(true);
+
+    // qDebug()<<"pro:"<<ui->lineEdit_dis_max->property("readOnly");
+    this->ui->button_modify_confirm->setText("修改");
+
+    QString tmp =  this->ui->lineEdit_dis_dream->text();
+    this->dis_dream = tmp.toInt();
+    qDebug()<<"dis_dream"<<this->dis_dream<<"\n";
+
+    tmp = this->ui->lineEdit_dis_max->text();
+    this->dis_max = tmp.toInt();
+    qDebug()<<"dis_max"<<this->dis_max<<"\n";
+
+    tmp = this->ui->lineEdit_dis_min->text();
+    this->dis_min = tmp.toInt();
+    qDebug()<<"dis_min"<<this->dis_min<<"\n";
+}
+
+void host::button_modify_clicked(void){
+    this->ui->lineEdit_dis_dream->setReadOnly(false);
+    this->ui->lineEdit_dis_max->setReadOnly(false);
+    this->ui->lineEdit_dis_min->setReadOnly(false);
+    this->ui->button_modify_confirm->setText("确认");
+}
+
+void host::button_modify_confirm_clicked(void){
+    if(this->ui->button_modify_confirm->text() == "修改"){
+        button_modify_clicked();
+    }else{
+        button_confirm_clicked();
+    }
+}
+
+
+void host::connect_func_signal(void){
+    connect(this->ui->button_modify_confirm,&QPushButton::clicked,this,&host::button_modify_confirm_clicked);
+
+
 }
--- src/host.h
+++ src/host.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "ui_host.h"
 #include <QMainWindow>
-
+#include "QString"
 class host : public QMainWindow {
     Q_OBJECT
     
@@ -9,6 +9,24 @@ public:
     host(QWidget* parent = nullptr);
     ~host();
 
+
+private:
+    
+
+    void setIntroduction(void);
+    
+    void button_confirm_clicked(void);
+    void button_modify_clicked(void);
+    void button_modify_confirm_clicked(void);
+
+    void connect_func_signal(void);
+
 private:
     Ui_host* ui;
+    QString version = "1.0";
+    int dis_dream;//mm
+    int dis_min;//mm
+    int dis_max;//max
+    bool status_connect;
+    
 };
